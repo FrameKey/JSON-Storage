fb83a541c7e39b29b6d44a7936a2fc7fcdd48b6a
FrameKey <106362041+FrameKey@users.noreply.github.com>
2022-06-25 21:48:07 -0300

    Add files via upload

---
+++ js/eventos.js
@@ -0,0 +1,62 @@
+let marcaAuto = document.querySelector("#autoSelection")    
+let operacion = document.querySelector("#operacionSelection")    
+let formulario = document.querySelector("#formulario")
+let btnSubmit = document.querySelector("#submit")
+let mensaje = document.querySelector("#mensaje")
+let ultimasBusquedas = document.querySelector("#ultimasBusquedas")
+let verMas = document.querySelector("#verMas")
+                    
+                    
+                    
+                  
+
+btnSubmit.addEventListener("click", ()=> {
+
+    if (infoAutos.find( p => p.marca.toLocaleLowerCase() === marcaAuto.value.toLocaleLowerCase()) && operacion.value.toLocaleLowerCase() == "seguro"){
+        let resultado = infoAutos.find( p => p.marca.toLocaleLowerCase() === marcaAuto.value.toLocaleLowerCase())
+        let seguro = parseInt(resultado.precio - (resultado.año * 3))
+      //   Swal.fire({
+      //    title: 'Genial!',
+      //    text: "El seguro del " + resultado.marca +" "+ resultado.modelo + " sale $" + seguro + " por mes.",
+      //    icon: 'success',
+      //    color:"#100423",
+      //    iconColor:"#00d9fc",
+      //    confirmButtonColor: "#fa0050",
+      //    confirmButtonText: 'Cool',
+      //    background:"#fff"
+
+      //     })
+    //  alert ("El seguro del " + resultado.marca +" "+ resultado.modelo + " sale $" + seguro + " por mes.")
+        mensaje.innerHTML = `<h2>El seguro del ${resultado.marca} ${resultado.modelo} sale $${seguro} por mes.</h2> `
+        //guardaBusqueda()
+        guardaBusquedaJSON()
+     } else if
+        (infoAutos.find( p => p.marca.toLocaleLowerCase() === marcaAuto.value.toLocaleLowerCase()) && operacion.value.toLocaleLowerCase() == "precio final"){
+        let resultado = infoAutos.find( p => p.marca.toLocaleLowerCase() === marcaAuto.value.toLocaleLowerCase())
+        let precioFinal = parseInt(resultado.precio * resultado.iva)
+        mensaje.innerHTML = `<h2>El precio final del ${resultado.marca} ${resultado.modelo} es de $${precioFinal} (IVA incluido).</h2> `  
+        //guardaBusqueda()
+        guardaBusquedaJSON()
+       // alert ("El precio final del " + resultado.marca +" "+ resultado.modelo + " es de $" + precioFinal + " (IVA incluido)")
+        } 
+        else if
+        ( marcaAuto.value.toLocaleLowerCase() === "" || operacion.value.toLocaleLowerCase() == ""){
+       // alert ("⚠️ Recuerda completar todos los campos")
+       mensaje.innerHTML = "<h2>⚠️ Recuerda completar todos los campos</h2>"
+        } 
+        else {
+           mensajeFinal()
+                }
+})
+
+
+verMas.addEventListener("click", ()=> {
+      mostrarUltimaBusquedaJSON()      
+})
+
+
+function mensajeFinal(){
+  mensaje.innerHTML = "<h2>Sorry, no tenemos ese auto 😔</h2>"
+}
+
+
